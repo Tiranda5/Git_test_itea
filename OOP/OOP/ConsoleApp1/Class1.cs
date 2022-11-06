7a02cce69b9c8958728b8c9f7f64e7f0105e3cbf
Tetiana Kazibrid <kazibridtetyana@gmail.com>
2022-11-06 16:42:17 +0200

    Вступ ООP

---
+++ OOP/OOP/ConsoleApp1/Class1.cs
@@ -0,0 +1,45 @@
+﻿using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+   public class Users
+    {
+        public string name;
+        public string password;
+        public string email;
+        public byte age;
+        public Admin Administrator = new Admin();
+        
+
+        public void setAll(string _name, string _email, string _pass, byte _age)
+        {
+            name = _name;
+            email = _email;
+            password = _pass;
+            age = _age;
+                
+        }
+
+        public void setEmail(string _email)
+        {  
+            email = _email;        
+        }
+
+        public void setAdmin(string role)
+        {
+            Administrator.role = role;
+
+        }
+
+        public void printAll()
+        {
+            Console.WriteLine($"Ім'я: {name}, email: {email}, пароль: {password}, вік: {age}");
+        }
+    }
+}
---
+++ OOP/OOP/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Users alex = new Users();
+            //alex.age = 45;
+            //alex.email = "test@gmail.com";
+            //alex.name = "Олексій";
+            alex.setAll("Олексій", "test@gmail.com", "1234", 45);
+            alex.setEmail("test2@gmail.com");
+
+            Users admin = new Users();
+            admin.setAdmin("Aдміністратор");
+            //admin.age = 22;
+            //admin.email = "admin@gmail.com";
+            //admin.name = "Адмін";
+            admin.setAll("Адмін", "admin@gmail.com", "1234", 22);
+            admin.setEmail("admin2@gmail.com");
+
+            admin.printAll();
+            alex.printAll();
+
+            Console.WriteLine(admin.Administrator.role);
+
+        }
+    }
+}
