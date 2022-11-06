4a526ef17a5193962b7a1cb6c903aa69e08e8536
Tetiana Kazibrid <kazibridtetyana@gmail.com>
2022-11-06 17:18:20 +0200

    Homework

---
+++ OOP/OOP_HW_ITP/OOP_HW_ITP/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace OOP_HW_ITP
+{
+    public class Car
+    {
+        public int weight;
+        public string model;
+        public char color;
+        public int speed;
+
+
+        public void printAll()
+        {
+            Console.WriteLine($"Модель: {model}, Вага: {weight}, Колір: {color}, Швидкість: {speed}");
+        }
+    }
+}
---
+++ OOP/OOP_HW_ITP/OOP_HW_ITP/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.Intrinsics.Arm;
+
+namespace OOP_HW_ITP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car Toyota = new Car();
+            Toyota.color = 'B';
+            Toyota.weight = 24;
+            Toyota.speed = 250;
+            Toyota.model = "CHR";
+
+
+            Toyota.printAll();
+        }
+    }
+}
