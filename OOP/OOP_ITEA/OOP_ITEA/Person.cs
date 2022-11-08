361e61783993b51999f990ba3ded5663541088ff
Tetiana Kazibrid <kazibridtetyana@gmail.com>
2022-11-08 19:41:41 +0200

    Work

---
+++ OOP/OOP_ITEA/OOP_ITEA/Person.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_ITEA
+{
+    internal class Person
+    {
+        string fam = string.Empty,
+            status = string.Empty,
+            health = string.Empty;
+        int age = 0,
+            salary = 0;
+        int count_ch = 0;
+
+        Person[] children = new Person[2];
+
+        //Read,Write-once
+        public string Fam
+        {
+            get { return fam; }
+            set { if (fam =="") fam = value; }
+        }
+
+        //Readonly
+
+        public string Status
+        {
+            get { return status; }
+        }
+
+        // Read - Write
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                age = value;
+                if (age < 7)
+                    status = "Child";
+                else if (age < 21)
+                    status = "Student";
+                else
+                    status = "Other";
+            }
+        }
+
+        //Write only
+        public int Salary
+        {
+            set { salary = value; }
+
+        }
+
+        public int Counth_ch
+        {
+            get { return count_ch; }    
+        }
+
+        public Person this[int i]
+        {
+            get
+            {
+                if (i > 0 && i < 3)
+                {
+                    return children[i];
+                }
+                else
+                {
+                    return children[0];
+                }
+            }
+            set
+            {
+                if (i == count_ch && i < 3)
+                {
+                    children[i] = value;
+                    count_ch++;
+                }
+        }
+        }
+    }
+}
---
+++ OOP/OOP_ITEA/OOP_ITEA/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OOP_ITEA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person_1 = new Person();
+            person_1.Fam = "John";
+            person_1.Age = 15;
+            person_1.Salary = 10000;
+
+            Person person_2 = new Person();
+            person_2.Fam = "Vasyl";
+            person_2.Age = 21;
+            person_2.Salary = 7000;
+
+            person_1[person_1.Counth_ch]=person_2;
+           // person_2.Fam = "Vasyl_2";
+
+            Console.WriteLine(person_1.Age + " " + person_1.Fam + " " + person_1.Status);
+            Console.WriteLine(person_2.Age + " " + person_2.Fam + " " + person_2.Status);
+            Console.WriteLine(person_1[0].Age + " " + person_1[0].Fam + " " + person_1[0].Status);
+        }
+    }
+}
